c2b56ff5f7391dbab989d26a821efe62c1d0a2c6
egor <goro4@mail.ru>
2024-07-22 14:16:19 +0500

    feat: add logging in file

--- src/main/java/org/dwh/Main.java
+++ src/main/java/org/dwh/Main.java
@@ -1,6 +1,12 @@
 package org.dwh;
 
+<<<<<<< HEAD
 
+=======
+import org.dwh.wokrer.RunAfterStartup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+>>>>>>> 72abcca (feat: add logging in file)
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.event.EventListener;
@@ -8,8 +14,15 @@ import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class Main {
+    static final Logger log = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+
+        log.debug("Main is a debug message");
+        log.info("Main is an info message");
+        log.warn("Main is a warn message");
+        log.error("Main is an error message");
+        log.trace("Main is an trace message");
     }
 
 
